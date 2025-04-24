6f0a6fc8b50cb253c2199becf88236fa6b56a785
Matthew Antone <matt.antone@gmail.com>
2025-04-24 12:43:03 -0700

    using vercel url for sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,7 +11,7 @@ export type SitemapLink = {
 
 const getURL = (page: Page, path: string = '') => {
   return {
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/${page.slug?.current || ""}`,
+    url: `${process.env.NODE_ENV === "production" ? process.env.VERCEL_URL || process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3000"}/${page.slug?.current || ""}`,
     lastModified: page._updatedAt || page.date,
     changeFrequency: 'monthly',
     priority: 0.8,
