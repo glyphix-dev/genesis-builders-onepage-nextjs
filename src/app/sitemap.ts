91a490703c9f04c6c21844b01b4d83a770c73433
Matthew Antone <matt.antone@gmail.com>
2025-04-24 12:39:14 -0700

    added sitemap and modified sitemap query

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { client } from "@/sanity/lib/client";
+import queries from "@/sanity/queries";
+import { Page } from '@/types/types.sanity';
+
+export type SitemapLink = {
+  url: string;
+  lastModified: string;
+  changeFrequency: string;
+  priority: number;
+}
+
+const getURL = (page: Page, path: string = '') => {
+  return {
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/${page.slug?.current || ""}`,
+    lastModified: page._updatedAt || page.date,
+    changeFrequency: 'monthly',
+    priority: 0.8,
+  } as SitemapLink;
+}
+
+export default async function sitemap(): Promise<SitemapLink[]> {
+  const links: SitemapLink[] = [];
+  const content = await client.fetch(queries.sitemap.links);
+  Object.keys(content).forEach((key) => {
+    content[key].forEach((page: Page) => {
+      links.push(getURL(page, key));
+    });
+  });
+  return links;
+}
--- src/sanity/queries/sitemap.ts
+++ src/sanity/queries/sitemap.ts
@@ -2,15 +2,23 @@ import type { SanityNextQueries } from "../../types/types.custom"
 
 export default {
   links: `{
+      "home": *[_type == "home"]{
+        title,
+        slug,
+        description,
+        "path": ""
+      },
       "pages": *[_type == "page"]{
         title,
         slug,
         description,
+        "path": ""
       },
       "posts": *[_type == "post"]{
         title,
         slug,
         description,
+        "path": "blog"
       }
     }`,
 } as const satisfies SanityNextQueries
