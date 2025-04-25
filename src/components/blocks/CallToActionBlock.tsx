a1ec4a1c28cba59d485c941578133e9f52e22cab
Matthew Antone <matt.antone@gmail.com>
2025-04-25 12:33:42 -0700

    fixed a build error

--- src/components/blocks/CallToActionBlock.tsx
+++ src/components/blocks/CallToActionBlock.tsx
@@ -24,7 +24,7 @@ const CallToActionBlock: React.FunctionComponent<ICallToActionBlockProps> = (pro
 export default CallToActionBlock;
 
 const DefaultLayout: React.FunctionComponent<ICallToActionBlockProps> = (props) => {
-  const { body, options } = props.value
+  const { body } = props.value
   return (
     <div className={cn(
       'flex flex-col gap-[var(--block-padding)] items-center justify-center md:justify-between text-center prose-h2:text-center',
