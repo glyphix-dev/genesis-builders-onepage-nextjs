25d08334ce0e10cf2886c99d62749b0cb5e3fb8b
Matthew Antone <matt.antone@gmail.com>
2025-04-25 11:37:26 -0700

    added robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import type { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/private/',
+    },
+    sitemap: `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/sitemap.xml`,
+  }
+}
