85acec54ff9fae239804063d0136baaf841436d8
Matthew Antone <matt.antone@gmail.com>
2025-04-26 11:01:27 -0700

    removed the defer

--- src/components/ContactFormExternal.tsx
+++ src/components/ContactFormExternal.tsx
@@ -9,7 +9,7 @@ interface IContactFormExternalProps {
 export function ContactFormExternal({ className }: IContactFormExternalProps) {
   return (
     <div className={cn("-mx-10", className)}>
-      <Script src="https://js-na2.hsforms.net/forms/embed/242562852.js" defer />
+      <Script src="https://js-na2.hsforms.net/forms/embed/242562852.js" />
       <div className="hs-form-frame" data-region="na2" data-form-id="f6b1ee86-1ff5-48db-96bb-cbab18c945a6" data-portal-id="242562852"></div>
     </div>
   )
