e69686d780e97a6cee2eda8be38131f6e30ea670
Matthew Antone <matt.antone@gmail.com>
2025-04-28 16:01:57 -0700

    using iframe instead

--- src/components/ContactFormExternal.tsx
+++ src/components/ContactFormExternal.tsx
@@ -1,16 +1,13 @@
 "use client"
 import * as React from "react"
-import Script from "next/script"
-import { cn } from "@/lib/utils"
+
 interface IContactFormExternalProps {
   className?: string;
 }
 
 export function ContactFormExternal({ className }: IContactFormExternalProps) {
   return (
-    <div className={cn("-mx-10", className)}>
-      <Script src="https://js-na2.hsforms.net/forms/embed/242562852.js" />
-      <div className="hs-form-frame" data-region="na2" data-form-id="f6b1ee86-1ff5-48db-96bb-cbab18c945a6" data-portal-id="242562852"></div>
-    </div>
+    <iframe src="https://share-na2.hsforms.com/29rHuhh_1SNuWu8urGMlFpg40eyp0" width="100%" height="600" className="w-full h-[1000px]">
+    </iframe>
   )
 }
