7287dedbe2d759e4dd2ffc2157510e699b1a1292
Matthew Antone <matt.antone@gmail.com>
2025-04-28 16:05:47 -0700

    removed unused variable

--- src/components/ContactFormExternal.tsx
+++ src/components/ContactFormExternal.tsx
@@ -5,7 +5,7 @@ interface IContactFormExternalProps {
   className?: string;
 }
 
-export function ContactFormExternal({ className }: IContactFormExternalProps) {
+export function ContactFormExternal() {
   return (
     <iframe src="https://share-na2.hsforms.com/29rHuhh_1SNuWu8urGMlFpg40eyp0" width="100%" height="600" className="w-full h-[1000px]">
     </iframe>
