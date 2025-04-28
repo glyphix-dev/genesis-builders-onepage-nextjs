32ef54ea35dbe54da2fe53af168a2a12e6e56cb9
Matthew Antone <matt.antone@gmail.com>
2025-04-28 16:18:58 -0700

    more precise form height

--- src/components/ContactFormExternal.tsx
+++ src/components/ContactFormExternal.tsx
@@ -3,7 +3,13 @@ import * as React from "react"
 
 export function ContactFormExternal() {
   return (
-    <iframe src="https://share-na2.hsforms.com/29rHuhh_1SNuWu8urGMlFpg40eyp0" width="100%" height="600" className="w-full h-[1000px]">
+    <iframe
+      src="https://share-na2.hsforms.com/29rHuhh_1SNuWu8urGMlFpg40eyp0"
+      width="100%"
+      height="600"
+      className="w-full h-[960px]"
+      title="Contact Form"
+    >
     </iframe>
   )
 }
