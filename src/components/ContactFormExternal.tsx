3404f7d8266a7484716df0a2c4e3c456f3582fa3
Matthew Antone <matt.antone@gmail.com>
2025-04-28 16:07:19 -0700

    removed interface

--- src/components/ContactFormExternal.tsx
+++ src/components/ContactFormExternal.tsx
@@ -1,10 +1,6 @@
 "use client"
 import * as React from "react"
 
-interface IContactFormExternalProps {
-  className?: string;
-}
-
 export function ContactFormExternal() {
   return (
     <iframe src="https://share-na2.hsforms.com/29rHuhh_1SNuWu8urGMlFpg40eyp0" width="100%" height="600" className="w-full h-[1000px]">
