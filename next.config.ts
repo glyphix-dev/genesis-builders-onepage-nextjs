60046f6bdc4177f8bd4453b08b732d22775cdf53
Matthew Antone <matt.antone@gmail.com>
2025-05-05 14:07:05 -0700

    added csp for iframes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+    frame-src 'self' https://share-na2.hsforms.com https://*.hsforms.com;
+    frame-ancestors 'self' https://share-na2.hsforms.com https://*.hsforms.com;
+    upgrade-insecure-requests;
+`;
+
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
@@ -8,6 +14,19 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.sanity.io',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
   },
 };
 
